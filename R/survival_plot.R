a9a26c7ce5449c7a0d7cb23bed8e0c4bd3f81dcf
Gabriel <gabriellopezmayer@gmail.com>
2024-11-30 17:07:01 -0500

    Initial commit of HealthViz R package

--- R/survival_plot.R
+++ R/survival_plot.R
@@ -9,11 +9,22 @@
 #' @return A Kaplan-Meier survival plot.
 #' @importFrom survival Surv survfit
 #' @importFrom survminer ggsurvplot
+#' @importFrom ggplot2 theme_minimal
 #' @export
 survival_plot <- function(data, time_col, status_col, group_col) {
 
+  # Check if the necessary columns exist in the data
+  if (!all(c(time_col, status_col, group_col) %in% names(data))) {
+    stop("The provided column names are not present in the data.")
+  }
+
+  # Convert status to a logical/numeric format if it's not
+  if (!is.numeric(data[[status_col]]) && !is.logical(data[[status_col]])) {
+    data[[status_col]] <- ifelse(data[[status_col]] == "alive", 1, 0)
+  }
+
   # Create a survival object using the Surv function
-  surv_object <- survival::Surv(data[[time_col]], data[[status_col]])
+  surv_object <- survival::Surv(time = data[[time_col]], event = data[[status_col]])
 
   # Fit the survival model based on the grouping factor
   fit <- survival::survfit(surv_object ~ data[[group_col]], data = data)
